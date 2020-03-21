b9f1c24f64d3bcefc11d4224dd731af7d06fe000
James Robinson <44409757+jakerdou@users.noreply.github.com>
2020-03-21 12:57:32 -0500

    added Get Top Tracks button

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import Spotify from 'spotify-web-api-js';
 
 const spot = new Spotify();
 
+//{tracks.map(track => track.artists.map(artist => <div>{artist.name}</div>))}
+
+
 class App extends React.Component {
   constructor(){
     super();
@@ -15,6 +18,12 @@ class App extends React.Component {
       nowPlaying: {
         name: "Not Checked Yet",
         image: ""
+      },
+      topTracks: {
+        items: [
+          {name: "Not Checked Yet"},
+          {artists: [{name: ""}]}
+        ]
       }
     }
 
@@ -56,7 +65,21 @@ class App extends React.Component {
       })
   }
 
+  getTopTracks() {
+    spot.getMyTopTracks()
+      .then((response) => {
+        //for some reason i cant read the artists
+        this.setState({
+          topTracks: {
+            items: response.items
+          }
+        })
+      })
+  }
+
   render(){
+    var tracks = this.state.topTracks.items;
+
     return (
       <div className="App">
         <a href = "http://localhost:8888">
@@ -69,6 +92,18 @@ class App extends React.Component {
         <button onClick = {() => this.getNowPlaying()}>
           Check What's Playing
         </button>
+
+        <div>
+          <button onClick = {() => this.getTopTracks()}>
+            Get Top Tracks
+          </button>
+          <div>My Top Tracks:</div>
+          <div>
+            {tracks.map(track =>
+              <div>{track.name}
+              </div>)}
+          </div>
+        </div>
       </div>
     );
   }
