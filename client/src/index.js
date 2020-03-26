95165ea55e56c5338be095bb9a336690a09ff15c
James Robinson <44409757+jakerdou@users.noreply.github.com>
2020-03-26 16:14:50 -0500

    fixing files not found

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import ArtistList from './ArtistList'
-import TrackList from './TrackList'
-import LogInBar from './LogInBar'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
